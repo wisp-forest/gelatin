0815fa15bcd1f8b22cecd9e89d625196b8c46d58
Dragon-Seeker <timekeeperguild@gmail.com>
2023-07-13 20:00:41 -0500

    Fix Translation Injection Mixin Injection point being incorrect

--- common/src/main/java/io/wispforest/gelatin/common/mixins/client/TranslationStorageMixin.java
+++ common/src/main/java/io/wispforest/gelatin/common/mixins/client/TranslationStorageMixin.java
@@ -19,7 +19,7 @@ import java.util.Map;
 @Mixin(TranslationStorage.class)
 public class TranslationStorageMixin {
 
-    @Inject(method = "load(Lnet/minecraft/resource/ResourceManager;Ljava/util/List;Z)Lnet/minecraft/client/resource/language/TranslationStorage;", at = @At(value = "RETURN"), locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(method = "load(Lnet/minecraft/resource/ResourceManager;Ljava/util/List;Z)Lnet/minecraft/client/resource/language/TranslationStorage;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap;copyOf(Ljava/util/Map;)Lcom/google/common/collect/ImmutableMap;"), locals = LocalCapture.CAPTURE_FAILHARD)
     private static void dynamicLangStuff(ResourceManager resourceManager, List<String> definitionCodes, boolean rightToLeft, CallbackInfoReturnable<TranslationStorage> cir, Map<String, String> map) {
         List<LanguageDefinition> definitions = List.copyOf(MinecraftClient.getInstance().getLanguageManager().getAllLanguages().values());
 
