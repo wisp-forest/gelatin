f6ed863b94431741db3e0f304f85334f25573d81
Dragon-Seeker <timekeeperguild@gmail.com>
2023-07-21 16:38:00 -0500

    Find Work around for loading the needed model data thru the ModelLoader's "STATIC_DEFINITIONS" map

--- dye-entries/src/main/java/io/wispforest/gelatin/dye_entries/client/DyeEntriesModelLoader.java
+++ dye-entries/src/main/java/io/wispforest/gelatin/dye_entries/client/DyeEntriesModelLoader.java
@@ -1,7 +1,7 @@
 package io.wispforest.gelatin.dye_entries.client;
 
+import com.google.common.collect.ImmutableMap;
 import io.wispforest.gelatin.common.misc.GelatinConstants;
-import io.wispforest.gelatin.dye_entries.ducks.ResourceFinderExtension;
 import io.wispforest.gelatin.dye_entries.mixins.client.model.ModelLoaderAccessor;
 import io.wispforest.gelatin.dye_entries.utils.DyeVariantBuilder;
 import io.wispforest.gelatin.dye_entries.variants.DyeableVariantRegistry;
@@ -15,56 +15,80 @@ import net.fabricmc.fabric.api.client.model.loading.v1.ModelResolver;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.block.BlockModels;
-import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.registry.Registries;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.*;
 
-public class DyeEntriesModelLoader implements ModelResolver /*, BlockStateResolver*/  {
+public class DyeEntriesModelLoader implements ModelResolver, BlockStateResolver  {
 
     public static final DyeEntriesModelLoader INSTANCE = new DyeEntriesModelLoader();
 
     private static final Set<DyeableBlockVariant> ALL_BLOCK_VARIANTS = new HashSet<>();
 
+    private static final Map<String, Identifier> BLOCKSTATE_ID_CACHE = new HashMap<>();
+
     public static void init(){
+        var STATIC_DEFS = ModelLoaderAccessor.gelatin$getSTATIC_DEFINITIONS();
+
+        Map<Identifier, StateManager<Block, BlockState>> mutableMap = new LinkedHashMap<>(STATIC_DEFS);
+
+        if (ALL_BLOCK_VARIANTS.isEmpty()) ALL_BLOCK_VARIANTS.addAll(DyeableVariantRegistry.getAllBlockVariants());
+
+        for (DyeableBlockVariant blockVariant : ALL_BLOCK_VARIANTS) {
+            String nameSpace = Objects.equals(blockVariant.variantIdentifier.getNamespace(), "minecraft") ? GelatinConstants.MODID : blockVariant.variantIdentifier.getNamespace();
+
+            Identifier baseId = new Identifier(nameSpace, "colored_" + blockVariant.variantIdentifier.getPath());
+
+            Block defaultEntry = blockVariant.getDefaultEntry();
+
+            Collection<Property<?>> properties = defaultEntry.getStateManager().getProperties();
+
+            StateManager<Block, BlockState> customStateManager = new StateManager.Builder<Block, BlockState>(defaultEntry)
+                    .add(properties.toArray(new Property[0]))
+                    .build(Block::getDefaultState, BlockState::new);
+
+            customStateManager.getStates().forEach(state -> {
+                ModelIdentifier identifier = BlockModels.getModelId(baseId, state);
+
+                BLOCKSTATE_ID_CACHE.put(baseId + identifier.getVariant(), identifier);
+            });
+
+            mutableMap.put(baseId, customStateManager);
+        }
+
+        ModelLoaderAccessor.gelatin$setSTATIC_DEFINITIONS(ImmutableMap.copyOf(mutableMap));
+
+        //---------------------------------
+
         ModelLoadingPlugin.register(pluginContext -> {
             pluginContext.resolveModel().register(INSTANCE);
 
-//            for (DyeableBlockVariant blockVariant : DyeableVariantRegistry.getAllBlockVariants()) {
-//                List<Block> blocks = new ArrayList<>();
-//
-//                Block defaultBlock = blockVariant.getDefaultEntry();
-//
-//                if(!defaultBlock.isDyed() && DyeVariantBuilder.shouldRedirectModelResource(Registries.BLOCK.getId(defaultBlock))){
-//                    blocks.add(defaultBlock);
-//                }
-//
-//                for (DyeColorant allColorant : DyeColorantRegistry.getAllColorants()) {
-//                    Block coloredBlock = blockVariant.getColoredEntry(allColorant);
-//
-//                    if(DyeVariantBuilder.shouldRedirectModelResource(Registries.BLOCK.getId(coloredBlock))){
-//                        blocks.add(coloredBlock);
-//                    }
-//                }
-//
-//                Block wrappedBlock = blocks.remove(0);
-//
-//                Map<BlockState, ModelIdentifier> statesToModelIDMap = new LinkedHashMap<>();
-//
-//                wrappedBlock.getStateManager().getStates().forEach(state -> statesToModelIDMap.put(state, BlockModels.getModelId(state)));
-//
-//                for (Map.Entry<BlockState, ModelIdentifier> entry : statesToModelIDMap.entrySet()) {
-//                    entry.getValue();
-//                }
-//
-//
-//                blocks.forEach(block -> pluginContext.registerBlockStateResolver(block, DyeEntriesModelLoader.INSTANCE));
-//            }
+            for (DyeableBlockVariant blockVariant : DyeableVariantRegistry.getAllBlockVariants()) {
+                List<Block> blocks = new ArrayList<>();
+
+                Block defaultBlock = blockVariant.getDefaultEntry();
+
+                if(!defaultBlock.isDyed() && DyeVariantBuilder.shouldRedirectModelResource(Registries.BLOCK.getId(defaultBlock))){
+                    blocks.add(defaultBlock);
+                }
+
+                for (DyeColorant allColorant : DyeColorantRegistry.getAllColorants()) {
+                    Block coloredBlock = blockVariant.getColoredEntry(allColorant);
+
+                    if(DyeVariantBuilder.shouldRedirectModelResource(Registries.BLOCK.getId(coloredBlock))){
+                        blocks.add(coloredBlock);
+                    }
+                }
+
+                blocks.forEach(block -> pluginContext.registerBlockStateResolver(block, DyeEntriesModelLoader.INSTANCE));
+            }
         });
     }
 
@@ -81,9 +105,7 @@ public class DyeEntriesModelLoader implements ModelResolver /*, BlockStateResolv
 
         Identifier baseEntryID = new Identifier(id.getNamespace(), isItemVersion ? id.getPath().replace("item/", "") : id.getPath());
 
-        if (!DyeVariantBuilder.shouldRedirectModelResource(baseEntryID)) return null;
-
-        if (!isItemVersion) return null;
+        if (!DyeVariantBuilder.shouldRedirectModelResource(baseEntryID) || !isItemVersion) return null;
 
         String[] stringParts = id.getPath().split("_");
 
@@ -99,17 +121,17 @@ public class DyeEntriesModelLoader implements ModelResolver /*, BlockStateResolv
             baseModelId = GelatinConstants.id("dynamic_dye");
         } else {
             for (DyeableBlockVariant blockVariant : ALL_BLOCK_VARIANTS) {
-                if (blockVariant.isSuchAVariant(id, false)) {
-                    DyeColorant dyeColorant = Registries.BLOCK.get(baseEntryID).getDyeColorant();
+                if (!blockVariant.isSuchAVariant(id, false)) continue;
 
-                    if (dyeColorant == DyeColorantRegistry.NULL_VALUE_NEW) return null;
+                DyeColorant dyeColorant = Registries.BLOCK.get(baseEntryID).getDyeColorant();
 
-                    String nameSpace = Objects.equals(blockVariant.variantIdentifier.getNamespace(), "minecraft") ? GelatinConstants.MODID : blockVariant.variantIdentifier.getNamespace();
+                if (dyeColorant == DyeColorantRegistry.NULL_VALUE_NEW) return null;
 
-                    baseModelId = new Identifier(nameSpace, "colored_" + blockVariant.variantIdentifier.getPath());
+                String nameSpace = Objects.equals(blockVariant.variantIdentifier.getNamespace(), "minecraft") ? GelatinConstants.MODID : blockVariant.variantIdentifier.getNamespace();
 
-                    break;
-                }
+                baseModelId = new Identifier(nameSpace, "colored_" + blockVariant.variantIdentifier.getPath());
+
+                break;
             }
         }
 
@@ -144,56 +166,52 @@ public class DyeEntriesModelLoader implements ModelResolver /*, BlockStateResolv
 
     //----------------------------------------------------------------------------------------------------------------------
 
-//    private final Map<String, ModelCacheInfo> BLOCK_MODEL_CACHE = new HashMap<>();
-//
-//    @Override
-//    public void resolveBlockStates(BlockStateResolver.Context context) {
-//        //--
-//        Block block = context.block();
-//
-//        if (ALL_BLOCK_VARIANTS.isEmpty()) ALL_BLOCK_VARIANTS.addAll(DyeableVariantRegistry.getAllBlockVariants());
-//
-//        Map<BlockState, ModelIdentifier> statesToModelIDMap = new LinkedHashMap<>();
-//
-//        block.getStateManager().getStates().forEach(state -> statesToModelIDMap.put(state, BlockModels.getModelId(state)));
-//
-//        for (Map.Entry<BlockState, ModelIdentifier> entry : statesToModelIDMap.entrySet()) {
-//            ModelIdentifier modelId = entry.getValue();
-//
-//            if (!DyeVariantBuilder.shouldRedirectModelResource(new Identifier(modelId.getNamespace(), modelId.getPath()))) {
-//                return;
-//            }
-//
-//            if(Objects.equals(modelId.getPath(), "slime_slab")) return;
-//
-//            for (DyeableBlockVariant blockVariant : ALL_BLOCK_VARIANTS) {
-//                if (!blockVariant.isSuchAVariant(block, false)) continue;
-//
-//                String nameSpace = Objects.equals(blockVariant.variantIdentifier.getNamespace(), "minecraft") ? GelatinConstants.MODID : blockVariant.variantIdentifier.getNamespace();
-//
-//                Identifier baseModelId = new Identifier(nameSpace, "colored_" + blockVariant.variantIdentifier.getPath());
-//
-//                UnbakedModel model;
-//
-//                String key = baseModelId + modelId.getVariant();
-//
-//                if (BLOCK_MODEL_CACHE.get(key) != null) {
-//                    model = BLOCK_MODEL_CACHE.get(key).model();
-//                } else {
-//                    ((ResourceFinderExtension) ModelLoader.BLOCK_STATES_FINDER).putTempRedirect(modelId, baseModelId);
-//
-//                    model = context.getOrLoadModel(modelId);
-//
-//                    BLOCK_MODEL_CACHE.put(key, new ModelCacheInfo(modelId, model));
-//                }
-//
-//                context.setModel(entry.getKey(), model);
-//            }
-//        }
-//
-//        //--
-//
-//    }
+    private final Map<String, ModelCacheInfo> BLOCK_MODEL_CACHE = new HashMap<>();
+
+    @Override
+    public void resolveBlockStates(BlockStateResolver.Context context) {
+        //--
+        Block block = context.block();
+
+        if (ALL_BLOCK_VARIANTS.isEmpty()) ALL_BLOCK_VARIANTS.addAll(DyeableVariantRegistry.getAllBlockVariants());
+
+        Map<BlockState, ModelIdentifier> statesToModelIDMap = new LinkedHashMap<>();
+
+        block.getStateManager().getStates().forEach(state -> statesToModelIDMap.put(state, BlockModels.getModelId(state)));
+
+        for (Map.Entry<BlockState, ModelIdentifier> entry : statesToModelIDMap.entrySet()) {
+            ModelIdentifier modelId = entry.getValue();
+
+            if (!DyeVariantBuilder.shouldRedirectModelResource(new Identifier(modelId.getNamespace(), modelId.getPath()))) {
+                return;
+            }
+
+            if(Objects.equals(modelId.getPath(), "slime_slab")) return;
+
+            for (DyeableBlockVariant blockVariant : ALL_BLOCK_VARIANTS) {
+                if (!blockVariant.isSuchAVariant(block, false)) continue;
+
+                String nameSpace = Objects.equals(blockVariant.variantIdentifier.getNamespace(), "minecraft") ? GelatinConstants.MODID : blockVariant.variantIdentifier.getNamespace();
+
+                Identifier baseModelId = new Identifier(nameSpace, "colored_" + blockVariant.variantIdentifier.getPath());
+
+                UnbakedModel model;
+
+                String key = baseModelId + modelId.getVariant();
+
+                if (BLOCKSTATE_ID_CACHE.get(key) != null) {
+                    model = new DelegatingUnbakedModel(BLOCKSTATE_ID_CACHE.get(key));
+                } else {
+                    throw new IllegalResolverStateException("A Block Variant was found to be missing the needed data to load models, which will cause massive issues! [Variant: " + blockVariant.variantIdentifier + "]");
+                }
+
+                context.setModel(entry.getKey(), model);
+            }
+        }
+
+        //--
+
+    }
 
     //----------------------------------------------------------------------------------------------------------------------
 
@@ -205,6 +223,12 @@ public class DyeEntriesModelLoader implements ModelResolver /*, BlockStateResolv
         @Nullable
         public ModelIdentifier modelId(){
             return isModelID() ? (ModelIdentifier) this.id() : null;
+        }
+    }
+
+    public static class IllegalResolverStateException extends IllegalStateException {
+        public IllegalResolverStateException(String s) {
+            super(s);
         }
     }
 }
