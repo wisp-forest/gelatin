ced3ee0afbdb6a098131ee15a48106ab1270e51a
Dragon-Seeker <timekeeperguild@gmail.com>
2023-07-21 16:38:00 -0500

    Find Work around for loading the needed model data thru the ModelLoader's "STATIC_DEFINITIONS" map

--- dye-entries/src/main/java/io/wispforest/gelatin/dye_entries/ducks/ResourceFinderExtension.java
+++
@@ -1,9 +0,0 @@
-package io.wispforest.gelatin.dye_entries.ducks;
-
-import net.minecraft.util.Identifier;
-
-public interface ResourceFinderExtension {
-
-    void putTempRedirect(Identifier key, Identifier value);
-
-}
--- dye-entries/src/main/java/io/wispforest/gelatin/dye_entries/mixins/client/model/ModelLoaderAccessor.java
+++ dye-entries/src/main/java/io/wispforest/gelatin/dye_entries/mixins/client/model/ModelLoaderAccessor.java
@@ -1,14 +1,25 @@
 package io.wispforest.gelatin.dye_entries.mixins.client.model;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
+import net.minecraft.state.StateManager;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Mixin(ModelLoader.class)
 public interface ModelLoaderAccessor {
+    @Accessor("STATIC_DEFINITIONS") static Map<Identifier, StateManager<Block, BlockState>> gelatin$getSTATIC_DEFINITIONS() { throw new UnsupportedOperationException(); }
+
+    @Mutable
+    @Accessor("STATIC_DEFINITIONS") static void gelatin$setSTATIC_DEFINITIONS(Map<Identifier, StateManager<Block, BlockState>> STATIC_DEFINITIONS) { throw new UnsupportedOperationException(); }
+
     @Invoker("loadModelFromJson") JsonUnbakedModel gelatin$LoadModelFromJson(Identifier id) throws IOException;
 }
